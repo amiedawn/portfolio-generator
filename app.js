974c034ad563eac4b85f72bd31caa69d1e7e0425
amiedawn <amie.d.carroll@gmail.com>
2020-11-17 20:41:32 -0700

    for la

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-const fs = require('fs');
+const { writeFile, copyFile} = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 const inquirer = require('inquirer');
 const promptUser = () => {
@@ -138,27 +137,20 @@ Add a New Project
 
 
 promptUser()
-.then(promptProject)
-.then(portfolioData => {
-  console.log(portfolioData);
-  
-  const pageHTML = generatePage(portfolioData);
-   fs.writeFile('./index.html', pageHTML, err => {
-     if (err) throw new Error(err);
-
-     console.log('Page created! Check out index.html in this directory to see it!');
-   });
-
-});
-   
-
-
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
